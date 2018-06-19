053d1f118bd746648fae38b5dcc574613fc8f2cb
Liam Moore <l.moore@cern.ch>
2018-06-19 21:14:42 +0200

    add reweight card for eft scan

---
+++ generate_ttbar_eft_LHEs/gen_ttbar_dim6.cmd
@@ -0,0 +1,7 @@
+import model dim6top_LO_UFO
+generate p p > t t~ QED=0, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ FCNC=0)
+output ttbar_dim6_dilepton
+generate p p > t t~ QED=0, (t > w+ b DIM6=0 FCNC=0, w+ > j j DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ FCNC=0)
+add process p p > t t~ QED=0, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > j j FCNC=0)
+output ttbar_dim6_semileptonic
+ 
