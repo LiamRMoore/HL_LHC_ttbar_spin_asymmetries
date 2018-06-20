d242e64adaa004e690b2952828665062a8fac904
LRMoore <lmoore@cmslapo.fynu.ucl.ac.be>
2018-06-20 13:07:59 +0200

    default to gen ~5M events

---
+++ generate_ttbar_eft_LHEs/launch_50_runs.cmd
@@ -0,0 +1,102 @@
+# go
+#
+# 1
+generate_events
+# 2
+generate_events
+# 3
+generate_events
+# 4
+generate_events
+# 5
+generate_events
+# 6
+generate_events
+# 7
+generate_events
+# 8
+generate_events
+# 9
+generate_events
+# 10
+generate_events
+# 11
+generate_events
+# 12
+generate_events
+# 13
+generate_events
+# 14
+generate_events
+# 15
+generate_events
+# 16
+generate_events
+# 17
+generate_events
+# 18
+generate_events
+# 19
+generate_events
+# 20
+generate_events
+# 21
+generate_events
+# 22
+generate_events
+# 23
+generate_events
+# 24
+generate_events
+# 25
+generate_events
+# 26
+generate_events
+# 27
+generate_events
+# 28
+generate_events
+# 29
+generate_events
+# 30
+generate_events
+# 31
+generate_events
+# 32
+generate_events
+# 33
+generate_events
+# 34
+generate_events
+# 35
+generate_events
+# 36
+generate_events
+# 37
+generate_events
+# 38
+generate_events
+# 39
+generate_events
+# 40
+generate_events
+# 41
+generate_events
+# 42
+generate_events
+# 43
+generate_events
+# 44
+generate_events
+# 45
+generate_events
+# 46
+generate_events
+# 47
+generate_events
+# 48
+generate_events
+# 49
+generate_events
+# 50
+generate_events
