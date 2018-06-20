20afeaf016150898ee14b25c715c65a5720b1234
LRMoore <lmoore@cmslapo.fynu.ucl.ac.be>
2018-06-20 12:43:13 +0200

    MC setup

--- generate_ttbar_eft_LHEs/gen_ttbar_dim6.cmd
+++ generate_ttbar_eft_LHEs/gen_ttbar_dim6.cmd
@@ -1,7 +1,15 @@
 import model dim6top_LO_UFO
-generate p p > t t~ QED=0, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ FCNC=0)
+generate p p > t t~ QED=0 DIM6=2, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ DIM6=0 FCNC=0) @0
+add process p p > t t~ j QED=0 DIM6=2, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ DIM6=0 FCNC=0) @1
 output ttbar_dim6_dilepton
-generate p p > t t~ QED=0, (t > w+ b DIM6=0 FCNC=0, w+ > j j DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ FCNC=0)
-add process p p > t t~ QED=0, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > j j FCNC=0)
-output ttbar_dim6_semileptonic
- 
+#
+# for dilepton need to merge separate charged final states
+# http://home.thep.lu.se/Pythia/pythia82html/CKKWLMerging.html#Definingthehardprocess
+# (LEPTONS/NEUTRINOS/BQUARKS containers won't cover both lepton charges since jets in ME final state)
+#
+generate p p > t t~ QED=0 DIM6=2, (t > w+ b DIM6=0 FCNC=0, w+ > j j DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ DIM6=0 FCNC=0) @0
+add process p p > t t~ j QED=0 DIM6=2, (t > w+ b DIM6=0 FCNC=0, w+ > j j DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > l- vl~ DIM6=0 FCNC=0) @1
+output ttbar_dim6_semileptonic_neg
+generate p p > t t~ QED=0 DIM6=2, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > j j DIM6=0 FCNC=0) @0
+add process p p > t t~ j QED=0 DIM6=2, (t > w+ b DIM6=0 FCNC=0, w+ > l+ vl DIM6=0 FCNC=0), (t~ > w- b~ DIM6=0 FCNC=0, w- > j j DIM6=0 FCNC=0) @1
+output ttbar_dim6_semileptonic_pos
---
+++ generate_ttbar_eft_LHEs/setup_lo_eft_1j_run.cmd
@@ -0,0 +1,31 @@
+# use as:  ./bin/madevent setup_100k_events_umeps.cmd
+#
+# move to card editing mode (configure run, but don't generate events yet)
+edit_cards
+# set auto-shower off so can run pythia separately
+shower=OFF
+# set detector sim off
+detector=OFF
+# set madanalysis off
+analysis=OFF
+# set reweight on so can regenerate samples with varied param choices
+reweight=ON
+# move to editing param/runcards
+done
+#
+set run_tag tt_dim6_100k
+# set 100k unweighted_events
+set nevents 100000
+# fixed ren/fac scales (for UMEPS merging)
+set fixed_ren_scale True
+set fixed_fac_scale True
+# turn off systematics studies (will use NLO systematics from NLO SM later)
+set use_syst False
+# will do CKKW-L/UMEPS merging, so switch off ickkw in run_card to 0 (see e.g. 'Merging in Madgraph 5 and Pythia 8, a brief overview')
+set ickkw 0
+#  loose minimum relative parton kT cut at matrix element level
+set xqcut 5
+# minimum jet pt to < the UMEPS merging scale (15 GeV) http://home.thep.lu.se/Pythia/pythia82html/UMEPSMerging.html
+set ptj 5
+# exit setting up Cards
+done
